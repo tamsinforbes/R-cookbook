474788e312ba5f14e71ef47d0c90ddc8d99b84aa
Tim Taylor <43499035+tjtnew@users.noreply.github.com>
2019-05-09 15:53:02 +0100

    changed build folder to defaul value

--- _deploy.sh
+++ _deploy.sh
@@ -21,7 +21,7 @@ then
   cd book-output
   git rm -rf *
   # Copy the new book and commit!
-  cp -r ../site/docs/* ./
+  cp -r ../_book/* ./
   git add --all *
   git commit -m "Travis built book"
   git push -q origin gh-pages
