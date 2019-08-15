3e90dbda194d7ae8c4a3eda50ec0dec7bc12f565
Tim Taylor <43499035+tjtnew@users.noreply.github.com>
2019-08-15 11:42:44 +0100

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]]
 then
   # Decrypt the deploy key
   openssl aes-256-cbc -K $encrypted_12aec5a48f60_key -iv $encrypted_12aec5a48f60_iv -in deploy_key.enc -out deploy_key -d
-  
+
   # Start ssh agent and add key
   eval "$(ssh-agent -s)" # Start the ssh agent
   chmod 600 deploy_key
@@ -15,18 +15,16 @@ then
   git config --global user.name "tamsinforbes"
 
   # clone the repository to the book-output directory
-  git clone -b gh-pages \	
-    git@github.com:departmentfortransport/R-cookbook.git \	
-    book-output	
-  cd book-output	
-  git rm -rf *	
-  # Copy the new book and commit!	
-  cp -r ../_book/* ./	
-  git add --all *	
-  git commit -m "Travis built book"	
-  git push -q origin gh-pages	
-else	
+  git clone -b gh-pages \
+    git@github.com:departmentfortransport/R-cookbook.git \
+    book-output
+  cd book-output
+  git rm -rf *
+  # Copy the new book and commit!
+  cp -r ../_book/* ./
+  git add --all *
+  git commit -m "Travis built book"
+  git push -q origin gh-pages
+else
   echo "Not publishing as not in MASTER branch"
 fi
-
-
